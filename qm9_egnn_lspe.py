1dbbeb3b158968575a151cbd3b6b52eb55f4ee28
iRaiyz <adam.nilav@gmail.com>
2023-05-06 14:22:57 +0200

    qm9 lspe

--- qm9_egnn_lspe.py
+++ qm9_egnn_lspe.py
@@ -13,24 +13,29 @@ from torch_geometric.transforms import RadiusGraph, AddRandomWalkPE, Compose
 class EGNNLayer(nn.Module):
     def __init__(self, num_hidden):
         super().__init__()
-        self.message_mlp = nn.Sequential(nn.Linear(2 * num_hidden + 1, num_hidden), nn.SiLU(), nn.Linear(num_hidden, num_hidden), nn.SiLU())
-        self.update_mlp = nn.Sequential(nn.Linear(2 * num_hidden, num_hidden), nn.SiLU(), nn.Linear(num_hidden, num_hidden))
+        self.message_mlp = nn.Sequential(nn.Linear(4 * num_hidden + 1, num_hidden), nn.SiLU(), nn.Linear(num_hidden, num_hidden), nn.SiLU())
+        self.message_mlp_pos = nn.Sequential(nn.Linear(2 * num_hidden, num_hidden), nn.Tanh(), nn.Linear(num_hidden, num_hidden), nn.Tanh())
+        self.update_mlp = nn.Sequential(nn.Linear(3 * num_hidden, num_hidden), nn.SiLU(), nn.Linear(num_hidden, num_hidden))
         self.edge_net = nn.Sequential(nn.Linear(num_hidden, 1), nn.Sigmoid())
         self.update_pos_net = nn.Sequential(nn.Linear(2*num_hidden, num_hidden), nn.Tanh(), nn.Linear(num_hidden, num_hidden), nn.Tanh())
 
     def forward(self, x, pos, edge_index, pe):
         send, rec = edge_index
-        state = torch.cat((x[send], x[rec], torch.linalg.norm(pos[send] - pos[rec], dim=1).unsqueeze(1)), dim=1)
-        state_pe = torch.cat([pe[send], pe[rec]], dim =1)
-        message = self.message_mlp(state, pe)
+
+        state = torch.cat((torch.cat([x[send], pe[send]], dim = -1), torch.cat([x[rec], pe[rec]], dim = -1), torch.linalg.norm(pos[send] - pos[rec], dim=1).unsqueeze(1)), dim=1)
+        state_pe = torch.cat([pe[send], pe[rec]], dim = 1)
+
+        message = self.message_mlp(state)
+        message_pos = self.message_mlp_pos(state_pe)
+
         # message = self.edge_net(message_pre) * message_pre
         aggr = scatter_add(message, rec, dim=0)
-        aggr_pos = scatter_add(state_pe, rec, dim = 0)
-        update = self.update_mlp(torch.cat((x, aggr), dim=1))
-        update_pe = self.update_pos_net(torch.cat([pe, aggr_pos], dim = -1))
-        
+        aggr_pos = scatter_add(message_pos, rec, dim = 0)
 
-        return update, pe
+        update = self.update_mlp(torch.cat((x, pe, aggr), dim = 1))
+        update_pe = self.update_pos_net(torch.cat([pe, aggr_pos], dim = 1))
+        
+        return update, update_pe
 
 
 class EGNN(nn.Module):
