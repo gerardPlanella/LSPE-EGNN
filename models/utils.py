ddeb8da70eddd687c404988606d6cae87fbaf024
lucapantea <luca26petru@gmail.com>
2023-05-16 14:53:06 +0200

    added deprecated method

--- models/utils.py
+++ models/utils.py
@@ -1,3 +1,6 @@
+import warnings
+import functools
+
 
 def get_pe_attribute(pe_name):
     if pe_name == 'rw':
@@ -6,3 +9,18 @@ def get_pe_attribute(pe_name):
         return 'laplacian_eigenvector_pe'
     else:
         raise NotImplementedError(f"PE method \"{pe_name}\" not implemented.")
+
+def deprecated(func):
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emitted
+    when the function is used."""
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+        warnings.warn("Call to deprecated function {}.".format(func.__name__),
+                      category=DeprecationWarning,
+                      stacklevel=2)
+        warnings.simplefilter('default', DeprecationWarning)  # reset filter
+        return func(*args, **kwargs)
+
+    return new_func
